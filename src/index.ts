82a1eb5a77a99a8aa01f19ae7f18940e9cc9c43c
Damian Rebolo <damianrebolo@gmail.com>
2025-02-21 21:18:13 +0100

    feat: add swagger

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,13 @@
 import { Elysia } from "elysia";
+import { swagger } from "@elysiajs/swagger";
 import postRoutes from "./routes/posts";
 
 const app = new Elysia();
 
-app.group("/api", (app) => app.use(postRoutes)).listen(3049);
+app
+  .use(swagger())
+  .group("/api", (app) => app.use(postRoutes))
+  .listen(3049);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
